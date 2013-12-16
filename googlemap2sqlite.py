fb0d87f8508899f6c9137acb3a263e95d729dac1
tpltnt <tpltnt.github@dropcut.net>
2013-12-16 19:13:08 +0100

    usage hint added

--- googlemap2sqlite.py
+++ googlemap2sqlite.py
@@ -4,6 +4,7 @@ import urllib.request   # to open URLs
 import sys              # for commandline arguments
 
 if 2 != len(sys.argv):
+    print("usage: " + str(sys.argv[0]) + " \"URL to google map\"")
     sys.exit(1)
 
 kmlrequest = urllib.request.urlopen(sys.argv[1] + '&output=kml')

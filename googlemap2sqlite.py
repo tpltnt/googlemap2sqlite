9789048d4e00a62aefa97f2e115705ed159a61ed
tpltnt <tpltnt.github@dropcut.net>
2013-12-16 19:25:59 +0100

    simple xml parsing

--- googlemap2sqlite.py
+++ googlemap2sqlite.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
-import urllib.request   # to open URLs
-import sys              # for commandline arguments
+import xml.etree.ElementTree as etree  # for XML parsing
+import urllib.request                  # to open URLs
+import sys                             # for commandline arguments
 
 if 2 != len(sys.argv):
     print("usage: " + str(sys.argv[0]) + " \"URL to google map\"")
     sys.exit(1)
 
 kmlrequest = urllib.request.urlopen(sys.argv[1] + '&output=kml')
-print(kmlrequest.read())
+kmlstring = kmlrequest.read()
+root = etree.fromstring(kmlstring)
+print(root.tag)

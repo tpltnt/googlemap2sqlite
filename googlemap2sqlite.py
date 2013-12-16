13891e210e2923074c482357caaccf69ca10cc61
tpltnt <tpltnt.github@dropcut.net>
2013-12-16 23:03:07 +0100

    crude data printing added

--- googlemap2sqlite.py
+++ googlemap2sqlite.py
@@ -18,5 +18,12 @@ except ValueError:
 root = etree.fromstring(kmlstring)
 list_of_places = root.findall("./{http://earth.google.com/kml/2.2}Document/{http://earth.google.com/kml/2.2}Placemark")
 for place in list_of_places:
-    print(place.tag)
+    # extract the name
+    element = place.find("./{http://earth.google.com/kml/2.2}name")
+    name = element.text.strip()
+    print(name)
+    # extract coordinates
+    element = place.find(".//{http://earth.google.com/kml/2.2}coordinates")
+    coordinates = element.text.strip()
+    print(coordinates)
 

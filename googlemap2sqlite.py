1180a225ea3d41bf9d230edb2c4bb27585bebd3d
tpltnt <tpltnt.github@dropcut.net>
2013-12-16 19:11:23 +0100

    URL as commandline

--- googlemap2sqlite.py
+++ googlemap2sqlite.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+
+import urllib.request   # to open URLs
+import sys              # for commandline arguments
+
+if 2 != len(sys.argv):
+    sys.exit(1)
+
+kmlrequest = urllib.request.urlopen(sys.argv[1] + '&output=kml')
+print(kmlrequest.read())
